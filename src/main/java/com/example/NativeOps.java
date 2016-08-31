0e0c3bac1480b6a3414452b90eeb103259ad6b57
raver119 <raver119@gmail.com>
2016-08-31 15:41:06 +0300

    more tests:
    - ordered and unordered dups
    
    - smaller broadcast
    
    - additional scalar test

--- src/main/java/com/example/NativeOps.java
+++ src/main/java/com/example/NativeOps.java
@@ -1,7 +1,10 @@
 package com.example;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.impl.scalar.ScalarAdd;
+import org.nd4j.linalg.api.ops.impl.scalar.ScalarMultiplication;
 import org.nd4j.linalg.api.ops.impl.transforms.Exp;
+import org.nd4j.linalg.cpu.nativecpu.ops.NativeOpExecutioner;
 import org.nd4j.linalg.factory.Nd4j;
 import org.openjdk.jmh.annotations.*;
 
@@ -17,14 +20,58 @@ public class NativeOps {
         INDArray array1 = Nd4j.linspace(1, 20480, 20480);
         INDArray array2 = Nd4j.linspace(1, 20480, 20480);
 
+        INDArray array3 = Nd4j.ones(128, 256);
+        INDArray arrayRow3 = Nd4j.linspace(1, 256, 256);
+
+        INDArray arrayUnordered = Nd4j.ones(512, 512);
+        INDArray arrayOrderedC = Nd4j.zeros(512, 512,'c');
+        INDArray arrayOrderedF = Nd4j.zeros(512, 512, 'f');
+
         {
             float sum = (float) array.sumNumber().doubleValue();
         }
     }
 
+
     @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void broadcastOp(SetupState state) throws IOException {
+    public void transformOp(SetupState state) throws IOException {
+        Nd4j.getExecutioner().exec(new Exp(state.array1, state.array2));
+    }
+
+
+
+    @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void scalarOp2(SetupState state) throws IOException {
+        Nd4j.getExecutioner().exec(new ScalarMultiplication(state.arrayUnordered, 2.5f));
+    }
+
+
+
+
+    @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void dupDifferentOrdersOp(SetupState state) throws IOException {
+        state.arrayUnordered.assign(state.arrayOrderedF);
+    }
+
+    @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void dupSameOrdersOp(SetupState state) throws IOException {
+        state.arrayUnordered.assign(state.arrayOrderedC);
+    }
+
+
+    @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void pairwiseOp1(SetupState state) throws IOException {
+        state.array1.addiRowVector(state.array2);
+    }
+
+    @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void broadcastOp2(SetupState state) throws IOException {
         state.array.addiRowVector(state.arrayRow);
+    }
+
+    @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void broadcastOp1(SetupState state) throws IOException {
+        state.array3.addiRowVector(state.arrayRow3);
     }
 
     @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.MICROSECONDS)
@@ -38,20 +85,8 @@ public class NativeOps {
     }
 
     @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void transformOp(SetupState state) throws IOException {
-        Nd4j.getExecutioner().exec(new Exp(state.array1, state.array2));
-    }
-
-    @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void pairwiseOp1(SetupState state) throws IOException {
-        state.array1.addiRowVector(state.array2);
-    }
-
-
-    @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public void scalarOp1(SetupState state) throws IOException {
         state.array2.addi(0.5f);
     }
-
 
 }
