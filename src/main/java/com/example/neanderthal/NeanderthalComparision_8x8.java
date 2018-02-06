3fd05363c37a22791c0a12f52004f26d49d5082f
Paul Dubs <paul.dubs@gmail.com>
2018-02-06 14:43:51 +0100

    Add MMULI for Neanderthal Comparison

---
+++ src/main/java/com/example/neanderthal/NeanderthalComparision_8x8.java
@@ -0,0 +1,25 @@
+package com.example.neanderthal;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class NeanderthalComparision_8x8 {
+
+
+    @State(Scope.Thread)
+    public static class SetupState {
+        public int size = 8;
+        public INDArray m1 = Nd4j.ones(size, size);
+        public INDArray m2 = Nd4j.ones(m1.shape());
+        public INDArray r = Nd4j.createUninitialized(m1.shape(), 'c');
+    }
+
+
+    @Benchmark @BenchmarkMode(Mode.SampleTime) @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void mmuli(SetupState state) {
+        state.m1.mmuli(state.m2, state.r);
+    }
+}
